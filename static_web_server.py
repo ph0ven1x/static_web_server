ea1742511ec0540dcf71dc33a951b0809a5e5c06
ph0ven1x <sshengbyi@gmail.com>
2024-03-23 14:25:18 +0800

    静态Web服务器-返回指定页面数据

--- static_web_server.py
+++ static_web_server.py
@@ -1,7 +1,9 @@
-# 返回固定页面
+# 返回指定页面
 import socket
+import urllib.parse
 
-if __name__ == '__main__':
+
+def main():
     # 创建tcp服务端套接字
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # 设置端口复用，程序退出，端口立即释放
@@ -14,22 +16,58 @@ if __name__ == '__main__':
         client_server_socket, ip_port = tcp_server_socket.accept()
         # 获取从客户端发来的数据
         recv_client_data = client_server_socket.recv(4096)
+
+        if len(recv_client_data) == 0:
+            print("浏览器关闭了")
+            client_server_socket.close()
+            return
+
         # 将发来的二进制数据解码
-        recv_client_content = recv_client_data.decode('utf-8')
+        # utf-8解码后还要进行url解码
+        recv_client_content = urllib.parse.unquote(recv_client_data.decode('utf-8'))
         print(recv_client_content)
+        # 分割请求行
+        request_list = recv_client_content.split(' ', maxsplit=2)
 
-        with open('./static/index.html', 'rb') as file:
-            # 读取文件内容
-            file_data = file.read()
+        # 获取请求资源路径
+        request_path = request_list[1]
+        print(request_path)
+        if request_path == '/':
+            request_path = '/index.html'
+        # 动态打开指定文件内
+        try:
+            with open('./static' + request_path, 'rb') as file:
+                # 读取文件内容
+                file_data = file.read()
+        except FileNotFoundError:
+            # 请求资源部不存在返回404数据
+            # 响应行
+            response_line = 'HTTP/1.1 404 Not Found\r\n'
+            # 响应头
+            response_header = 'Server:PWS1.0\r\n'
+            with open('./static/error.html', 'rb') as file:
+                file_data = file.read()
 
-        # 相应行
-        response_line = 'HTTP/1.1 200 OK\r\n'
-        # 响应头
-        response_header = 'Server:PWS1.0\r\nContent-Type: text/html\r\n\r\n'
-        # 响应体
-        response_body = file_data
-        # 拼接响应报文
-        response_data = (response_line + response_header + '\r\n').encode('utf-8') + response_body
-        # 发送响应数据
-        client_server_socket.send(response_data)
-        client_server_socket.close()
+            # 响应体
+            response_body = file_data
+            # 拼接响应报文
+            response_data = (response_line + response_header + '\r\n').encode('utf-8') + response_body
+            client_server_socket.send(response_data)
+        else:
+            # 响应行
+            response_line = 'HTTP/1.1 200 OK\r\n'
+            # 响应头
+            response_header = 'Server:PWS1.0\r\n'
+            # 响应体
+            response_body = file_data
+            # 拼接响应报文
+            response_data = (response_line + response_header + '\r\n').encode('utf-8') + response_body
+            # 发送响应数据
+            client_server_socket.send(response_data)
+            client_server_socket.close()
+        finally:
+            client_server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
